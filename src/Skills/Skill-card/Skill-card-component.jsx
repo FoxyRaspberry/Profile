571795382621a8afcb4a4779144246d13f6ec724
FoxyRaspberry <hogwartsgsrh97@gmail.com>
2025-11-07 15:52:55 +0300

    feat: add styles to project cards

--- src/Skills/Skill-card/Skill-card-component.jsx
+++ src/Skills/Skill-card/Skill-card-component.jsx
@@ -1,5 +1,5 @@
 import './Skill-card-component.css';
-import { skillsCard } from './skills-card.js'
+// import { skillsCard } from './skills-card.js'
 
 export function SkillCard({image, name, value}) {
 
