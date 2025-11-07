a6aa250dd68eb90587bf58398ce9f2a376e5a752
FoxyRaspberry <hogwartsgsrh97@gmail.com>
2025-11-07 16:20:45 +0300

    feat: adjust the visually noticeable styles for cards

--- src/Projects/Project-card/Project-card-component.jsx
+++ src/Projects/Project-card/Project-card-component.jsx
@@ -1,10 +1,9 @@
 import './project-card-component.css';
-// import { projectsCard } from './project-card.js'
 
 export function ProjectCard({image, title, subtitle, link}) {
 
   return (
-    <li className='projects-section__project-card projects-section__project-card--gradient-border'>
+    <li className='projects-section__project-card'>
       <img className='projects-section__project-card-image' src={image} alt="project" />
       <h4 className='projects-section__project-card-title'>{title}</h4>
       <p className='projects-section__project-card-subtitle'>{subtitle}</p>
