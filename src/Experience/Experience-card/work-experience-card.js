85dff8719b8501be61fa4f28e6c7e0155f5c2eb0
FoxyRaspberry <hogwartsgsrh97@gmail.com>
2025-11-10 15:59:32 +0300

    feat: sort `workExperienceCard`

--- src/Experience/Experience-card/work-experience-card.js
+++ src/Experience/Experience-card/work-experience-card.js
@@ -2,6 +2,8 @@ export const workExperienceCard = [
   {
     id: 1,
     period: '09.2022 - 04.2023',
+    periodFrom: '2022-09',
+    periodTo: '2023-04',
     title: 'Course "UpSkillMe" from the company "EPAM"',
     subtitle: 'A course from the international company EPAM helped me acquire basic HTML, CSS, and JS skills.',
   },
@@ -9,6 +11,8 @@ export const workExperienceCard = [
   {
     id: 2,
     period: '04.2022 - 05.2022',
+    periodFrom: '2022-04',
+    periodTo: '2022-05',
     title: 'HTML Academy and HTML and CSS Tutorials at w3schools',
     subtitle: 'Helped me solidify my acquired knowledge.',
   },
@@ -16,6 +20,8 @@ export const workExperienceCard = [
   {
     id: 3,
     period: '05.2023 - 09.2023',
+    periodFrom: '2023-05',
+    periodTo: '2023-09',
     title: 'RS Schools Course "JavaScript/Front-end"',
     subtitle: 'This course provided a deeper understanding of JavaScript and front-end development.',
   },
@@ -23,6 +29,8 @@ export const workExperienceCard = [
   {
     id: 4,
     period: '11.2025 - present',
+    periodFrom: '2025-11',
+    periodTo: null,
     title: 'React Developer Course from Praktikum',
     subtitle: 'I am exploring the React ecosystem, writing my own web app, and mastering Typescript and other useful tools.',
   },
@@ -31,14 +39,37 @@ export const workExperienceCard = [
   {
     id: 5,
     period: '04.2023 - present',
+    periodFrom: '2023-04',
+    periodTo: null,
     title: 'Independent development',
     subtitle: 'Responsive website design of varying complexity based on Figma layouts, commissioned by clients.',
   },
 
   {
     id: 6,
-    period: '11.10.2023 - 18.09.2025',
+    period: '10.2023 - 09.2025',
+    periodFrom: '2023-10',
+    periodTo: '2025-09',
     title: 'A large company engaged in marketing',
     subtitle: 'Developed and optimized websites (html, css, js), developed landing pages on WordPress.',
   },
 ];
+
+workExperienceCard.sort((a, b) => {
+  const periodFromDifference = parsePeriodFrom(b.periodFrom) - parsePeriodFrom(a.periodFrom);
+  if (periodFromDifference !== 0) {
+    return periodFromDifference;
+  }
+
+  const periodToDifference = parsePeriodTo(b.periodTo) - parsePeriodTo(a.periodTo);
+  return !Number.isNaN(periodToDifference) ? periodToDifference : 0;
+});
+
+
+function parsePeriodFrom(periodFrom) {
+  return Date.parse(periodFrom)
+}
+
+function parsePeriodTo(periodTo) {
+  return periodTo !== null ? Date.parse(periodTo) : Infinity
+}
