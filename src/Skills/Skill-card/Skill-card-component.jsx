a81bebdb4d4a57457d3a8fa4163e70bfcb21bdd9
FoxyRaspberry <hogwartsgsrh97@gmail.com>
2025-11-14 13:12:15 +0300

    refactor: rename classes for `SkillCard`

--- src/Skills/Skill-card/Skill-card-component.jsx
+++ src/Skills/Skill-card/Skill-card-component.jsx
@@ -2,9 +2,9 @@ import './Skill-card-component.css';
 
 export function SkillCard({ image, name }) {
   return (
-    <div className='skills-section__skill-card'>
-      <img className='skills-section__skill-card-image' src={image} alt="skill" />
-      <span className='skills-section__skill-card-name'>{name}</span>
+    <div className='skill-card__container'>
+      <img className='skill-card__image' src={image} alt="skill" />
+      <span className='skill-card__title'>{name}</span>
       {/* <progress className='skills-section__skill-card-progress' value={value} max="100"></progress> */}
     </div>
   );
